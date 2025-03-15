745bd812360ea3a1dff14ca677fcea0087b64ba9
Max Miller <67687590+maxtmiller@users.noreply.github.com>
2025-03-15 15:22:32 -0400

    update

--- main.py
+++ main.py
@@ -162,8 +162,10 @@ def register():
 def logout():
     """Log user out"""
 
+    # Forget any user_id
     session.clear()
 
+    # Redirect user to login form
     return redirect("/login")
 
 
@@ -187,6 +189,153 @@ def home():
     except Exception as e:
         print("Firestore Error:", e)
         return redirect("/login") 
+
+
+@app.route("/classify", methods=["POST"])
+@login_required
+def classify():
+    """Classify Skin Cancer from Model"""
+
+    user_id = session["user_id"]
+
+    data = request.get_json()
+    image_data = data.get("image")
+
+    image_data = image_data.split(',')[1]
+    image_bytes = base64.b64decode(image_data)
+    image = Image.open(BytesIO(image_bytes))
+
+    img_byte_arr = io.BytesIO()
+    image.save(img_byte_arr, format='PNG')
+    img_byte_arr.seek(0)
+
+    vals = classification_model(img_byte_arr)
+
+    image_path = f"images/{user_id}/{str(hash(image_data))}.png"
+
+    image_url = upload_image(image_bytes, image_path, bucket)
+
+    result_data = {
+        "user_id": user_id,
+        "classification": vals[0],
+        "accuracy": str(vals[1]),
+        "image_url": image_url,
+    }
+
+    db.collection("images").add(result_data)
+
+    if (vals[0]=="malignant"):
+        flash("Skin likely Malignant!")
+        return redirect("/help")
+
+    flash("Skin likely Benign!")
+    return redirect("/images")
+
+
+@app.route("/help")
+@login_required
+def help():
+    """Help Page"""
+
+    user_id = session["user_id"]
+    user_ref = db.collection("users").document(user_id)
+    user_doc = user_ref.get()
+    user = user_doc.to_dict()
+
+    return render_template("help.html", user=user)
+
+
+@app.route("/map")
+@login_required
+def map():
+    """Logic for finding nearby hospitals"""
+
+    user_id = session["user_id"]
+    user_ref = db.collection("users").document(user_id)
+    user_doc = user_ref.get()
+    user = user_doc.to_dict()
+
+    return render_template("map.html", user=user)
+
+
+@app.route("/chatbot")
+@login_required
+def chatbot():
+    """Logic for chatbot page"""
+
+    user_id = session["user_id"]
+    user_ref = db.collection("users").document(user_id)
+    user_doc = user_ref.get()
+    user = user_doc.to_dict()
+
+    return render_template("chatbot.html", user=user)
+
+
+@app.route("/chatbot/message", methods=["POST"])
+@login_required
+def chatbot_message():
+
+    user_message = request.json["message"]
+
+    bot_response = cohere_chat(user_message)
+
+    return jsonify({"response": bot_response})
+
+
+@app.route("/images")
+@login_required
+def images():
+    """Display Images"""
+
+    user_id = session["user_id"]
+    
+    user_ref = db.collection("users").document(user_id)
+    user_doc = user_ref.get()
+    user = user_doc.to_dict()
+
+    images_ref = db.collection("images")
+    images_query = images_ref.where("user_id", "==", user_id)
+    images_docs = images_query.stream()
+
+    images_data = []
+    for image_doc in images_docs:
+        image_data = image_doc.to_dict()
+        image_data["id"] = image_doc.id
+        
+        image_data["image_url"] = image_data.get("image_url")
+        
+        images_data.append(image_data)
+
+    return render_template("images.html", images=images_data, user=user)
+
+
+@app.route("/settings", methods=["GET", "POST"] )
+@login_required
+def settings():
+    """Settings Page"""
+
+    user_id = session["user_id"]
+    
+    user_ref = db.collection("users").document(user_id)
+    user_doc = user_ref.get()
+    user = user_doc.to_dict()
+    
+    return render_template("settings.html",  user=user)
+
+
+@app.route("/about")
+@login_required
+def about():
+    """About Page"""
+
+    user_id = session["user_id"]
+
+    user_ref = db.collection("users").document(user_id)
+    user_doc = user_ref.get()
+    user = user_doc.to_dict()
+
+
+    return render_template("about.html",  user=user)
 
 
 if __name__ == "__main__":
